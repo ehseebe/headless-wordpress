cfa31a5a930ba1017cff7478a8c113b1d4f6d49f
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-10-14 16:45:43 -0400

    Get and set notes data

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function NoteList() {
 
+  const [notes, setNotes] = useState(null);
+
+  useEffect(() => {
+    axios.get("/wp-json/wp/v2/posts")
+    .then(res => 
+      setNotes(res.data))
+    .catch((err) => console.log(err.message));
+  }, []);
   
+  console.log("notes?", notes)
+
 
   return (
     <div>
