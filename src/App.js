b09dcee872b7460fd81ef67085c6a0d2dbe32262
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-10-19 16:25:44 -0400

    Renamed, removed old files

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, { useState } from 'react';
 import Axios from 'axios';
 import './App.css';
 
-// import NewNote from './components/NewNote';
-import ViewNote from './components/ViewNote';
-import NoteList from './components/NoteList';
-
-
 function App() {
 
   // Axios.get("/wp-json/wp/v2/posts").then(res => console.log(res.data))
 
-  // get should be made here and pass props to components
-  // show viewNote on click of list item
-  // open text field to write note easily/more reminder set up
-  // use ACF to show completed true/false
   return (
     <div className="App">
       YAS
-      {/* <ViewNote />
-      <NoteList /> */}
     </div>
   );
 }
